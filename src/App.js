e4465867b4ca1133b178ae54369828f577475c67
PremanandChowdhury <freudigprem@gmail.com>
2021-10-08 02:14:36 +0530

    feat: Added Accordion

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from 'react'
+import Accordion from './components/Accordion'
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a Javascript Library.',
+  },
+  {
+    title: 'What is the Advantage of React?',
+    content: 'Easy to use. React can be used with other Frameworks.',
+  },
+  {
+    title: 'What are React Hooks?',
+    content:
+      "Hooks introduced to help React's functional component to use States.",
+  },
+]
 
 export default function App() {
-  return <h1>Widget App Component</h1>
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  )
 }
