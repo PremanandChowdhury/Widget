9a1aef9d75db8cad5f119fe4c8ffe4364da85afd
PremanandChowdhury <freudigprem@gmail.com>
2021-10-09 00:47:49 +0530

    code: Added the state for the Active Index

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const [activeIndex, setActiveIndex] = useState(null)
+
+  const onHandleClick = (index) => {
+    return <h1>{setActiveIndex(index + 1)}</h1>
+  }
+
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className='title active'>
+        <div className='title active' onClick={() => onHandleClick(index)}>
           <i className='dropdown icon'></i>
           {item.title}
         </div>
@@ -14,7 +20,12 @@ const Accordion = ({ items }) => {
       </React.Fragment>
     )
   })
-  return <div className='ui styled accordion'>{renderedItems}</div>
+  return (
+    <div className='ui styled accordion'>
+      {renderedItems}
+      {activeIndex}
+    </div>
+  )
 }
 
 export default Accordion
