39d89a59263f8448c0c0357c37f03ccdd8807a91
PremanandChowdhury <freudigprem@gmail.com>
2021-10-10 22:34:42 +0530

    code: Opening the acitve item in accordian

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -4,28 +4,25 @@ const Accordion = ({ items }) => {
   const [activeIndex, setActiveIndex] = useState(null)
 
   const onHandleClick = (index) => {
-    return <h1>{setActiveIndex(index + 1)}</h1>
+    return <h1>{setActiveIndex(index)}</h1>
   }
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : ''
+
     return (
       <React.Fragment key={item.title}>
-        <div className='title active' onClick={() => onHandleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onHandleClick(index)}>
           <i className='dropdown icon'></i>
           {item.title}
         </div>
-        <div className='content active'>
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     )
   })
-  return (
-    <div className='ui styled accordion'>
-      {renderedItems}
-      {activeIndex}
-    </div>
-  )
+  return <div className='ui styled accordion'>{renderedItems}</div>
 }
 
 export default Accordion
