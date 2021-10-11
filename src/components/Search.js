2e7eb261617b0b448e514351110935d5fd06528f
PremanandChowdhury <freudigprem@gmail.com>
2021-10-11 23:31:29 +0530

    code: Added 3 ways to access external api in UseEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,38 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Search = () => {
   const [term, setTerm] = useState('')
+
+  const URL =
+    'en.wikipedia.org/w/api.php?cation=query&list=search&format=json&srsearch=SEARCHTERM'
 
   const handleChange = (e) => {
     return setTerm(e.target.value)
   }
 
   useEffect(() => {
-    console.log('USE-EFFECT CALLED!')
+    /**
+     * Using Asynchronous Code in UseEffect with async and await
+     * This is recommended by the React Developers
+     * Way 1: With Function Expression
+     */
+    const search = async () => {
+      await axios.get(URL)
+    }
+
+    /**
+     * Way 2: With IFFE
+     */
+    ;(async () => await axios.get(URL))()
+
+    /**
+     * Way 3: With promises
+     */
+    axios
+      .get(URL)
+      .then((response) => console.log(response.data))
+      .catch((err) => console.log(err))
   }, [term])
 
   return (
