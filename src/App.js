e6e80d2ebeb500c67bd0ff07ed088355c3c9b9f2
PremanandChowdhury <freudigprem@gmail.com>
2021-10-11 23:30:43 +0530

    refact: Refactoring the App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react'
-import Accordion from './components/Accordion'
+// import Accordion from './components/Accordion'
+import Search from './components/Search'
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a Javascript Library.',
-  },
-  {
-    title: 'What is the Advantage of React?',
-    content: 'Easy to use. React can be used with other Frameworks.',
-  },
-  {
-    title: 'What are React Hooks?',
-    content:
-      "Hooks introduced to help React's functional component to use States.",
-  },
-]
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a Javascript Library.',
+//   },
+//   {
+//     title: 'What is the Advantage of React?',
+//     content: 'Easy to use. React can be used with other Frameworks.',
+//   },
+//   {
+//     title: 'What are React Hooks?',
+//     content:
+//       "Hooks introduced to help React's functional component to use States.",
+//   },
+// ]
 
 export default function App() {
   return (
     <div>
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   )
 }
