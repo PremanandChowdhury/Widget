400a810cb97052c691c2c91d38c99a8e23897561
PremanandChowdhury <freudigprem@gmail.com>
2021-10-19 23:35:11 +0530

    feat: XSS attack & Show individual page

--- src/components/Search.js
+++ src/components/Search.js
@@ -47,9 +47,17 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className='item'>
+        <div className='right floated content'>
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className='ui button'>
+            Go to page
+          </a>
+        </div>
         <div className='content'>
           <div className='header'>{result.title}</div>
-          {result.snippet}
+          {/* Rendering a string to HTML using dangerouslySetInnerHTML in react */}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
         </div>
       </div>
     )
