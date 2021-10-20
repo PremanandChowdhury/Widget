e7e194417d3c69e56bda6cd9872d3ce7aaff87bf
PremanandChowdhury <freudigprem@gmail.com>
2021-10-21 00:22:23 +0530

    mock: Created Mock data

---
+++ src/mock/data.js
@@ -0,0 +1,30 @@
+export const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a Javascript Library.',
+  },
+  {
+    title: 'What is the Advantage of React?',
+    content: 'Easy to use. React can be used with other Frameworks.',
+  },
+  {
+    title: 'What are React Hooks?',
+    content:
+      "Hooks introduced to help React's functional component to use States.",
+  },
+]
+
+export const options = [
+  {
+    label: 'This is Red Color',
+    value: 'red',
+  },
+  {
+    label: 'This is Green Color',
+    value: 'green',
+  },
+  {
+    label: 'This is Orange Color',
+    value: 'orange',
+  },
+]
