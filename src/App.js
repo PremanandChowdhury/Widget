a92bb18dfe49a7cd4372e7c14ee4ee7f6e5e6d49
PremanandChowdhury <freudigprem@gmail.com>
2021-10-22 23:55:36 +0530

    feat: Dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import Accordion from './components/Accordion'
 import Dropdown from './components/Dropdown'
 // import Search from './components/Search'
@@ -6,11 +6,16 @@ import Dropdown from './components/Dropdown'
 import { items, options } from './mock/data'
 
 export default function App() {
+  const [selected, setSelected] = useState(options[0])
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   )
 }
