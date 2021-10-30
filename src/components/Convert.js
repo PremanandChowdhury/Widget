95d7916df1e1d4749269579783f4b0b81d09b23c
PremanandChowdhury <freudigprem@gmail.com>
2021-10-30 21:42:21 +0530

    feat: Convert Component Scaffolding

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 const KEY = process.env.REACT_APP_GOOGLE_TRANSLATE_KEY
 
 const Convert = ({ language, text }) => {
-  console.log('the key is : ', KEY)
   useEffect(() => {
-    axios.post('https://translation.googleapis.com/language/translate/v2', {
-      params: {
-        q: text,
-        target: language.value,
-        key: KEY,
-      },
-    })
+    axios.post(
+      'https://translation.googleapis.com/language/translate/v2',
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: KEY,
+        },
+      }
+    )
   }, [language, text])
   return <div />
 }
