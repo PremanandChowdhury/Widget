a875d0935f57ae287f3a94f69c874f8e132e74e0
PremanandChowdhury <freudigprem@gmail.com>
2021-11-01 23:42:19 +0530

    feat: Working Convert Language component and displaying in UI

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,22 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 const KEY = process.env.REACT_APP_GOOGLE_TRANSLATE_KEY
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('')
+
   useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: KEY,
-        },
-      }
-    )
-  }, [language, text])
-  return <div />
+    const translateText = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: KEY,
+          },
+        }
+      )
+      setTranslated(data.data.translations[0].translatedText)
+    }
+    translateText()
+  }, [language, text, translated])
+
+  return (
+    <div>
+      <h1 className='ui header'>{translated}</h1>
+    </div>
+  )
 }
 
 export default Convert
