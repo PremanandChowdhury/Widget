8a783977128c74c006bb674bdf581628286c3ac4
PremanandChowdhury <freudigprem@gmail.com>
2021-11-03 23:44:52 +0530

    feat: Creating Route Component + Adding the Route to App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
 import React, { useState } from 'react'
-// import Accordion from './components/Accordion'
-// import Dropdown from './components/Dropdown'
-// import Search from './components/Search'
+import Accordion from './components/Accordion'
+import Dropdown from './components/Dropdown'
+import Search from './components/Search'
 import Translate from './components/Translate'
-// eslint-disable-next-line no-unused-vars
 import { items, options } from './mock/data'
+import Route from './components/Route'
 
 export default function App() {
-  // const [selected, setSelected] = useState(options[0])
-  // const [showDropdown, setShowDropDown] = useState(true)
+  const [selected, setSelected] = useState(options[0])
+  const [showDropdown, setShowDropDown] = useState(true)
 
   return (
-    <div>
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      {/* <button onClick={() => setShowDropDown(!showDropdown)}>
-        Toggle ShowDropdown
-      </button>
-
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        />
-      ) : null} */}
-      <Translate />
+    <div style={{ margin: '0 10px' }}>
+      <Route path='/'>
+        <Accordion items={items} />
+      </Route>
+      <Route path='/list'>
+        <button onClick={() => setShowDropDown(!showDropdown)}>
+          Toggle ShowDropdown
+        </button>
+        {showDropdown ? (
+          <Dropdown
+            label='Select the color'
+            selected={selected}
+            onSelectedChange={setSelected}
+            options={options}
+          />
+        ) : null}
+      </Route>
+      <Route path='/search'>
+        <Search />
+      </Route>
+      <Route path='/translate'>
+        <Translate />
+      </Route>
     </div>
   )
 }
